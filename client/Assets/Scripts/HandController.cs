9cd1fd8e37f296b35a32c9cd5ecf4b514c458a16
guushertogh <31773433+guushertogh@users.noreply.github.com>
2023-11-05 16:40:49 +0100

    initial scene

---
+++ client/Assets/Scripts/HandController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Kaart;
+using UnityEngine;
+
+private 
+
+public class HandController : MonoBehaviour
+{
+    public GameObject handObject;
+    public float offset;
+
+    public float xStart;
+    public float yStart;
+    public float zStart; 
+    public int aantalKaartenZichtbaar;
+    private List<Kaart> kaarten;
+
+    private int kijkIndex;
+
+    private List<int> zichtbareKaarten;
+
+    private List<int> geselecteerdeKaarten;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        kaarten = new List<Kaart>{
+            new Kaart{
+                Kleur = 1,
+                Rang = 2
+            },
+            new Kaart{
+                Kleur = 2,
+                Rang = 10
+            },
+            new Kaart{
+                Kleur = 4,
+                Rang = 13
+            }
+        };
+        selectedPosition = kaarten.Count-1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        zichtbareKaarten = UpdateZichtbareKaarten();
+        LoadZichtbareKaarten();
+        
+    }
+
+    private List<int> UpdateZichtbareKaarten(){
+        var result = new List<int>();
+        if (kaarten.Count<=0) 
+            return result;
+        if (kaarten.Count<=aantalKaartenZichtbaar){
+            for(int i = 0; i++; i < kaarten.Count){
+                result.Add(i);
+            }
+            return result;
+        }
+    }
+
+    private void LoadZichtbareKaarten(){
+        float xposition = xStart;
+        foreach(Kaart k in zichtbareKaarten){
+            
+        }
+    }
+}
---
+++ client/Assets/Scripts/Models/Kaart.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kaart{
+    public class Kaart
+    {
+        public int Kleur {get; init;}
+        public int Rang {get; init;}    
+    }
+}
